e49ecbf8890582b1193d34c065d965e491a8a2ed
Akshay <40245850+akshayguruprasad@users.noreply.github.com>
2018-12-13 05:20:45 +0530

    Update setup_jenkins.sh

--- bin/setup_jenkins.sh
+++ bin/setup_jenkins.sh
@@ -12,41 +12,55 @@ REPO=$2
 CLUSTER=$3
 echo "Setting up Jenkins in project ${GUID}-jenkins from Git Repo ${REPO} for Cluster ${CLUSTER}"
 
-# Set up Jenkins with sufficient resources
-# 
-#oc new-project ${GUID}-jenkins - see setup-projects.sh
-oc new-app jenkins-persistent --param ENABLE_OAUTH=true --param MEMORY_LIMIT=2Gi --param VOLUME_CAPACITY=4Gi --param DISABLE_ADMINISTRATIVE_MONITORS=true
+
+TMPL_DIR=$(dirname $0)/../templates
+
+# Code to set up the Jenkins project to execute the
+# three pipelines.
+# This will need to also build the custom Maven Slave Pod
+# Image to be used in the pipelines.
+# Finally the script needs to create three OpenShift Build
+# Configurations in the Jenkins Project to build the
+# three micro services. Expected name of the build configs:
+# * mlbparks-pipeline
+# * nationalparks-pipeline
+# * parksmap-pipeline
+# The build configurations need to have two environment variables to be passed to the Pipeline:
+# * GUID: the GUID used in all the projects
+# * CLUSTER: the base url of the cluster used (e.g. na39.openshift.opentlc.com)
+
+# To be Implemented by Student
 
 
-# Create custom agent container image with skopeo
-# 
-oc new-build  -D $'FROM docker.io/openshift/jenkins-agent-maven-35-centos7:v3.11\n
-      USER root\nRUN yum -y install skopeo && yum clean all\n
-      USER 1001' --name=jenkins-agent-appdev -n ${GUID}-jenkins
+#Setting up Jenkins project base on Jenkins Template build on the course with Pavel
+echo "Setting up Jenkins in project ${GUID}-jenkins from Git Repo ${REPO} for Cluster ${CLUSTER}"
 
-# Create pipeline build config pointing to the ${REPO} with contextDir `openshift-tasks`
-# 
-echo "apiVersion: v1
-items:
-- kind: "BuildConfig"
-  apiVersion: "v1"
-  metadata:
-    name: "tasks-pipeline"
-  spec:
-    source:
-      type: "Git"
-      git:
-        uri: "${REPO}"
-      contextDir: "openshift-tasks"
-    strategy:
-      type: "JenkinsPipeline"
-      jenkinsPipelineStrategy:
-        jenkinsfilePath: Jenkinsfile
-        env:
-          - name: "GUID"
-            value: "${GUID}"
-kind: List
-metadata: []" | oc create -f - -n ${GUID}-jenkins
+# add a sleep time to help use better the resources since the grade pipeline send the 5 setup project simmultaneously
+#sleep 240
+
+oc new-app -f ${TMPL_DIR}/hgp-jenkins.yaml -n $GUID-jenkins
+oc rollout status dc/jenkins -w -n $GUID-jenkins
+
+echo "Configuring Jenkins Slave Maven"
+oc new-app -f ${TMPL_DIR}/hgp-jenkins-configmap.yaml --param GUID=${GUID} -n ${GUID}-jenkins
+
+#Setting up jenkins pipelines on Openshift 
+echo "Creating and configuring Build Configs for 3 pipelines"
+oc new-build ${REPO} --name="mlbparks-pipeline" --strategy=pipeline --context-dir="MLBParks" -n $GUID-jenkins
+oc set env bc/mlbparks-pipeline CLUSTER=${CLUSTER} GUID=${GUID} -n $GUID-jenkins
+
+oc new-build ${REPO} --name="nationalparks-pipeline" --strategy=pipeline --context-dir="Nationalparks" -n $GUID-jenkins
+oc set env bc/nationalparks-pipeline CLUSTER=${CLUSTER} GUID=${GUID} -n $GUID-jenkins
+
+oc new-build ${REPO} --name="parksmap-pipeline" --strategy=pipeline --context-dir="ParksMap" -n $GUID-jenkins
+oc set env bc/parksmap-pipeline CLUSTER=${CLUSTER} GUID=${GUID} -n $GUID-jenkins
+
+#Delete the empty build created by default
+sleep 10
+oc delete build/mlbparks-pipeline-1 -n $GUID-jenkins
+oc delete build/nationalparks-pipeline-1 -n $GUID-jenkins
+oc delete build/parksmap-pipeline-1 -n $GUID-jenkins
+
 # Make sure that Jenkins is fully up and running before proceeding!
 while : ; do
   echo "Checking if Jenkins is Ready..."
@@ -58,4 +72,5 @@ while : ; do
   echo "...no. Sleeping 10 seconds."
   sleep 10
 done
+
 
